fe008b420cc9f0e379e592f391967bb964079bc6
Jakub Kolber <115322984+jcb16@users.noreply.github.com>
2023-11-29 21:45:14 +0100

    done

--- Szyfr Polibiusza/MainWindow.xaml.cs
+++ Szyfr Polibiusza/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Windows;
@@ -29,215 +30,97 @@ namespace Szyfr_Polibiusza
             DecryptedTextBlock.Text = decryptedText;
         }
     }
+
     public static class PolibiusCipher
     {
-        static string polibiusAlphabet = "ABCDEFGHIKLMNOPRSTUVWXYZĄĆĘŁŃÓŚŹŻ";
-        static int tableSize = 6;
-
         public static string Encrypt(string plaintext, int shift)
         {
-            //plaintext = RemoveDiacritics(plaintext.ToUpper());
-            //string encryptedText = "";
 
-            //foreach (char c in plaintext)
-            //{
-            //    if (c == 'J')
-            //        encryptedText += "24 ";
-            //    else if (c == 'Ó')
-            //        encryptedText += "54 ";
-            //    else if (Char.IsLetter(c))
-            //    {
-            //        int index = (polibiusAlphabet.IndexOf(c) + shift) % polibiusAlphabet.Length;
-            //        int row = index / tableSize;
-            //        int col = index % tableSize;
-            //        encryptedText += (row + 1).ToString() + (col + 1).ToString() + " ";
-            //    }
-            //    else if (c == ' ')
-            //    {
-            //        encryptedText += " ";
-            //    }
-            //}
+            char[,] polibiuszTable = {
+            {'A', 'Ą', 'B', 'C', 'Ć', 'D'},
+            {'E', 'Ę', 'F', 'G', 'H', 'I'},
+            {'J', 'K', 'L', 'Ł', 'M', 'N'},
+            {'Ń','O', 'Ó', 'P', 'Q', 'R'},
+            {'S','Ś','T', 'U', 'V', 'W'},
+            {'X','Y','Z', 'Ź', 'Ż', ' ',}
+            };
 
-            //return encryptedText.Trim();
-            plaintext = RemoveDiacritics(plaintext.ToUpper());
-            string encryptedText = "";
+            plaintext = plaintext.ToUpper();
+            List<string> encryptedTextList = new List<string>();
 
-            foreach (char c in plaintext)
+            foreach (char character in plaintext)
             {
-                if (Char.IsLetter(c))
+                if (character == 'J')
                 {
-                    int index = (polibiusAlphabet.IndexOf(c) + shift) % polibiusAlphabet.Length;
-                    int row = index / tableSize + 1;
-                    int col = index % tableSize + 1;
-                    encryptedText += row.ToString() + col.ToString() + " ";
+                    encryptedTextList.Add("24");
                 }
-                else if (Char.IsWhiteSpace(c))
+                else if (character == ' ')
                 {
-                    // Jeśli to spacja, dodaj spację
-                    encryptedText += " ";
+                    encryptedTextList.Add(" ");
                 }
                 else
                 {
-                    // Jeśli to nie litera ani spacja, dodaj bez zmian
-                    encryptedText += c;
+                    bool found = false;
+                    for (int i = 0; i < polibiuszTable.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < polibiuszTable.GetLength(1); j++)
+                        {
+                            if (character == polibiuszTable[i, j])
+                            {
+                                int row = (i + shift) % polibiuszTable.GetLength(0);
+                                int col = (j + shift) % polibiuszTable.GetLength(1);
+                                encryptedTextList.Add($"{row}{col}");
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found) break;
+                    }
                 }
             }
 
-            return encryptedText.Trim();
+            return string.Join(" ", encryptedTextList);
         }
 
         public static string Decrypt(string ciphertext, int shift)
         {
-            string[] pairs = ciphertext.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            string decryptedText = "";
+            char[,] polibiuszTable = {
+            {'A', 'Ą', 'B', 'C', 'Ć', 'D'},
+            {'E', 'Ę', 'F', 'G', 'H', 'I'},
+            {'J', 'K', 'L', 'Ł', 'M', 'N'},
+            {'Ń','O', 'Ó', 'P', 'Q', 'R'},
+            {'S','Ś','T', 'U', 'V', 'W'},
+            {'X','Y','Z', 'Ź', 'Ż', ' ',}
+            };
 
-            foreach (string pair in pairs)
+
+            // Usunięcie spacji i podzielenie tekstu na pary znaków
+            string[] cipherParts = ciphertext.Split(' ');
+            List<string> decryptedTextList = new List<string>();
+
+            foreach (string cipherPart in cipherParts)
             {
-                if (pair.Length == 2)
+                if (cipherPart == " ")
                 {
-                    int row = int.Parse(pair[0].ToString()) - 1;
-                    int col = int.Parse(pair[1].ToString()) - 1;
-
-                    int index = row * tableSize + col - shift;
-                    while (index < 0)
-                    {
-                        index += polibiusAlphabet.Length;
-                    }
-
-                    char decryptedChar = polibiusAlphabet[index % polibiusAlphabet.Length];
-
-                    if (decryptedChar == 'J')
-                    {
-                        decryptedText += "J";
-                    }
-                    else if (decryptedChar == 'Ó')
-                    {
-                        decryptedText += "Ó";
-                    }
-                    else
-                    {
-                        decryptedText += decryptedChar;
-                    }
+                    decryptedTextList.Add(" ");
                 }
-                else if (pair == " ")
+                else
                 {
-                    decryptedText += " ";
+                    int row = int.Parse(cipherPart[0].ToString());
+                    int col = int.Parse(cipherPart[1].ToString());
+
+                    // Uwzględnienie przesunięcia i zapewnienie prawidłowych indeksów
+                    row = (row - shift + polibiuszTable.GetLength(0)) % polibiuszTable.GetLength(0);
+                    col = (col - shift + polibiuszTable.GetLength(1)) % polibiuszTable.GetLength(1);
+
+                    decryptedTextList.Add(polibiuszTable[row, col].ToString());
                 }
             }
 
-            return decryptedText;
-        }
-
-        public static string RemoveDiacritics(string text)
-        {
-            string normalized = text.Normalize(NormalizationForm.FormD);
-            StringBuilder result = new StringBuilder();
-
-            foreach (char c in normalized)
-            {
-                if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
-                {
-                    result.Append(c);
-                }
-            }
-
-            return result.ToString();
+            return string.Join("", decryptedTextList);
         }
     }
 
-    //public static class PolibiusCipher
-    //{
-    //    static char[,] polibiusTable = new char[6, 6]
-    //    {
-    //        { 'A', 'Ą', 'B', 'C', 'Ć', 'D' },
-    //        { 'E', 'Ę', 'F', 'G', 'H', 'I' },
-    //        { 'J', 'K', 'L', 'Ł', 'M', 'N' },
-    //        { 'O', 'Ó', 'P', 'Q', 'R', 'S' },
-    //        { 'T', 'U', 'V', 'W', 'X', 'Y' },
-    //        { 'Z', 'Ź', 'Ż', ' ', ' ', ' ' }
-    //    };
-
-    //    public static string Encrypt(string plaintext, int shift)
-    //    {
-    //        plaintext = RemoveDiacritics(plaintext.ToUpper());
-    //        string encryptedText = "";
-
-    //        foreach (char c in plaintext)
-    //        {
-    //            if (c == 'J')
-    //                encryptedText += "24";
-    //            else if (Char.IsLetter(c))
-    //            {
-    //                bool found = false; // Flaga, aby określić, czy litera została znaleziona w tabeli
-    //                for (int row = 0; row < 6; row++)
-    //                {
-    //                    for (int col = 0; col < 6; col++)
-    //                    {
-    //                        if (polibiusTable[row, col] == c)
-    //                        {
-    //                            row = (row + shift) % 6;
-    //                            col = (col + shift) % 6;
-    //                            encryptedText += (row + 1).ToString() + (col + 1).ToString() + " ";
-    //                            found = true;
-    //                            break;
-    //                        }
-    //                    }
-    //                    if (found) break;
-    //                }
-    //                if (!found)
-    //                {
-    //                    // Jeśli litera nie została znaleziona, dodaj ją bez zmian
-    //                    encryptedText += c;
-    //                }
-    //            }
-    //            else if (c == ' ')
-    //            {
-    //                encryptedText += " ";
-    //            }
-    //        }
-
-    //        return encryptedText.Trim();
-    //    }
-
-    //    public static string Decrypt(string ciphertext, int shift)
-    //    {
-    //        string decryptedText = "";
-    //        string[] pairs = ciphertext.Split(' ');
-
-    //        foreach (string pair in pairs)
-    //        {
-    //            if (pair.Length == 2)
-    //            {
-    //                int row = int.Parse(pair[0].ToString()) - 1;
-    //                int col = int.Parse(pair[1].ToString()) - 1;
-    //                row = (row - shift + 6) % 6;
-    //                col = (col - shift + 6) % 6;
-    //                decryptedText += polibiusTable[row, col];
-    //            }
-    //            else if (pair == " ")
-    //            {
-    //                decryptedText += " ";
-    //            }
-    //        }
-    //        return decryptedText;
-    //    }
-
-    //    public static string RemoveDiacritics(string text)
-    //    {
-    //        string normalized = text.Normalize(NormalizationForm.FormD);
-    //        StringBuilder result = new StringBuilder();
-
-    //        foreach (char c in normalized)
-    //        {
-    //            if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
-    //            {
-    //                result.Append(c);
-    //            }
-    //        }
-
-    //        return result.ToString();
-    //    }
-    //}
 
 
 
